6176b1776b1456deff75014ec9ee77b49ba3bb3c
Matin <matin.damyar@gmail.com>
2024-02-28 15:49:59 -0500

    done

--- d.ts
+++ d.ts
@@ -9,115 +9,81 @@ export type DefaultSomethingnewnewServerSelection = {
     readonly updatedAt: true;
     readonly something: true;
 };
-/** All the data passed to an effect or precondition within the `create` action on the `somethingnewnew` model. */
+/** Context of an `actionType: "create"` on the `somethingnewnew` model. */
 export interface CreateSomethingnewnewActionContext extends AmbientContext {
     /**
-    * The model of the record this action is operating on
+    * model this action is operating on
     */
-    model: NotYetTyped;
+    model: somethingnewnew;
+
     /**
-    * The `somethingnewnew` record this action is operating on.
-    */
-    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
-    /**
-    * An object passed between all preconditions and effects of an action execution at the `scope` property.
-    * Useful for transferring data between effects.
-    */
-    scope: ActionExecutionScope;
-    /**
-    * An object describing what started this action execution.
+    * object specifying the trigger to this action (e.g. api call, webhook events etc.)
     */
     trigger: ActionTrigger;
+
     /**
-    * An object containing all the incoming params that have been defined for this action. Includes params added by any triggers, as well as custom params defined in the action.
+    * object containing the incoming data(this models fields) passed by triggers or user inputs
     */
     params: {};
+
     /**
-    * The context of this action. This context does not have a defined inner context.
+    * record object initiated
+    */
+    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
+    
+    /**
+    * @private
     */
     context: CreateSomethingnewnewActionContext;
 }
-/** All the data passed to an effect or precondition within the `update` action on the `somethingnewnew` model. */
+
+/** Context of an `actionType: "update"` on the `somethingnewnew` model. */
 export interface UpdateSomethingnewnewActionContext extends AmbientContext {
     /**
-    * The model of the record this action is operating on
+    * The model this action is operating on
     */
-    model: NotYetTyped;
+    model: somethingnewnew;
+    
     /**
-    * The `somethingnewnew` record this action is operating on.
-    */
-    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
-    /**
-    * An object passed between all preconditions and effects of an action execution at the `scope` property.
-    * Useful for transferring data between effects.
-    */
-    scope: ActionExecutionScope;
-    /**
-    * An object describing what started this action execution.
+    * object specifying the trigger to this action (e.g. api call, webhook events etc.)
     */
     trigger: ActionTrigger;
+    
     /**
-    * An object containing all the incoming params that have been defined for this action. Includes params added by any triggers, as well as custom params defined in the action.
+    * object containing the incoming data(this models fields) and record ID passed by triggers or user inputs
     */
     params: {};
+    
     /**
-    * The context of this action. This context does not have a defined inner context.
+    * record object specified by ID to be updated
+    */
+    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
+    
+    /**
+    * @private
     */
     context: UpdateSomethingnewnewActionContext;
 }
-/** All the data passed to an effect or precondition within the `delete` action on the `somethingnewnew` model. */
+
+/** Context of an `actionType: "delete"` on the `somethingnewnew` model. */
 export interface DeleteSomethingnewnewActionContext extends AmbientContext {
     /**
-    * The model of the record this action is operating on
+    * The model this action is operating on
     */
-    model: NotYetTyped;
+    model: somethingnewnew;
+    
     /**
-    * The `somethingnewnew` record this action is operating on.
-    */
-    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
-    /**
-    * An object passed between all preconditions and effects of an action execution at the `scope` property.
-    * Useful for transferring data between effects.
-    */
-    scope: ActionExecutionScope;
-    /**
-    * An object describing what started this action execution.
+    * object specifying the trigger to this action (e.g. api call, webhook events etc.)
     */
     trigger: ActionTrigger;
+
     /**
-    * An object containing all the incoming params that have been defined for this action. Includes params added by any triggers, as well as custom params defined in the action.
+    * record object specified by ID to be deleted
     */
-    params: {};
+    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
+
     /**
-    * The context of this action. This context does not have a defined inner context.
+    * @private
     */
     context: DeleteSomethingnewnewActionContext;
-}
-/** All the data passed to an effect or precondition within the `asd` action on the `somethingnewnew` model. */
-export interface AsdSomethingnewnewActionContext extends AmbientContext {
-    /**
-    * The model of the record this action is operating on
-    */
-    model: NotYetTyped;
-    /**
-    * The `somethingnewnew` record this action is operating on.
-    */
-    record: GadgetRecord<Select<Somethingnewnew, DefaultSomethingnewnewServerSelection>>;
-    /**
-    * An object passed between all preconditions and effects of an action execution at the `scope` property.
-    * Useful for transferring data between effects.
-    */
-    scope: ActionExecutionScope;
-    /**
-    * An object describing what started this action execution.
-    */
-    trigger: ActionTrigger;
-    /**
-    * An object containing all the incoming params that have been defined for this action. Includes params added by any triggers, as well as custom params defined in the action.
-    */
-    params: {};
-    /**
-    * The context of this action. This context does not have a defined inner context.
-    */
-    context: AsdSomethingnewnewActionContext;
 }
